4ba8ed99774bf6a0bbd1e58fb5c7899d41799336
PavlovskyiK <nesteemusic@gmail.com>
2024-02-11 23:03:41 +0200

    remove comments

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,3 @@
-// declare module "*.vue" {
-//     import type { DefineComponent } from 'vue'
-//     const component: DefineComponent<{}, {}, any>
-//     export default component
-// }
 
 declare module "vuex" {
     export * from "vuex/types/index.d.ts";
